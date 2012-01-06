67253d3d1bb770a4ff788ce84a829610c7401e32
Juan A. Diaz <juan@linux.org.ar>
2012-01-06 14:51:13 -0300

    Agregado icono para el configlet de disqus.
    Más algunos cambios cosmeticos.

--- src/telesur/registry/controlpanel.py
+++ src/telesur/registry/controlpanel.py
@@ -8,7 +8,10 @@ from telesur.registry.interfaces import IDisqusSettings
 from plone.z3cform import layout
 
 class DisqusControlPanelForm(RegistryEditForm):
+    """ Disqus setting configlet for the control panel.
+    """
     schema = IDisqusSettings
 
-DisqusControlPanelView = layout.wrap_form(DisqusControlPanelForm, ControlPanelFormWrapper)
+DisqusControlPanelView = layout.wrap_form(
+    DisqusControlPanelForm, ControlPanelFormWrapper)
 DisqusControlPanelView.label = u"Disqus settings"
