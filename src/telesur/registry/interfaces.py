2f3913c851f10faf2253ccaaa94c3fbfae12827a
hvelarde <hector.velarde@gmail.com>
2012-01-19 20:08:13 -0600

    pep8/pyflakes

--- src/telesur/registry/interfaces.py
+++ src/telesur/registry/interfaces.py
@@ -6,25 +6,25 @@ from zope.interface import Interface
 
 from telesur.registry import _
 
+
 class IDisqusSettings(Interface):
     """ Disqus settings. Credentials for access to the disqus api.
         Obtainable via plone.registry.
     """
     access_token = schema.TextLine(
-        title = _(u'Access Token'),
-        description = _(u'Access token to retrive information from the disqus forum.'),
-        required = True,
+        title=_(u'Access Token'),
+        description=_(u'Access token to retrive information from the disqus forum.'),
+        required=True,
         )
 
     app_public_key = schema.TextLine(
-        title = _(u'Application public key'),
-        description = _(u'public key'),
-        required = True,
+        title=_(u'Application public key'),
+        description=_(u'public key'),
+        required=True,
         )
 
     app_secret_key = schema.TextLine(
-        title = _(u'Application secret key'),
-        description = _(u'secret key'),
-        required = True,
+        title=_(u'Application secret key'),
+        description=_(u'secret key'),
+        required=True,
         )
-
